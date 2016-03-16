a1dc26a71e2eb13ff138a4f94dd7bc0c02fc8380
treelite <c.xinle@gmail.com>
2016-03-16 17:22:42 +0800

    and

---
+++ test/andSpec.js
@@ -0,0 +1,53 @@
+/**
+ * @file add spec
+ * @author treelite(c.xinle@gmail.com)
+ */
+
+import CPU from '../lib/CPU';
+import {FLAG_MASK} from '../lib/const';
+
+describe('and', () => {
+
+    let memory = new Buffer(0x100);
+    let cpu = new CPU(memory);
+
+    beforeEach(() => {
+        memory.fill(0);
+        cpu.reset();
+    });
+
+    it('immediate to accumulator', () => {
+        cpu.setPSW(FLAG_MASK.OF, 1);
+        cpu.setPSW(FLAG_MASK.CF, 1);
+        cpu.setPSW(FLAG_MASK.AF, 1);
+
+        cpu.al = 0x82;
+        // Add al 0x12
+        memory.writeUInt8(0x24, 0);
+        memory.writeUInt8(0x90, 1);
+
+        cpu.next();
+        expect(cpu.ip).toEqual(2);
+        expect(cpu.al).toEqual(0x82 & 0x90);
+        expect(cpu.psw & FLAG_MASK.OF).toEqual(0);
+        expect(cpu.psw & FLAG_MASK.CF).toEqual(0);
+        expect(cpu.psw & FLAG_MASK.AF).not.toEqual(0);
+        expect(cpu.psw & FLAG_MASK.SF).not.toEqual(0);
+        expect(cpu.psw & FLAG_MASK.ZF).toEqual(0);
+        expect(cpu.psw & FLAG_MASK.PF).not.toEqual(0);
+    });
+
+    it('register to memory', () => {
+        cpu.al = 0xAB;
+        memory.writeUInt8(0x11, 0x10);
+        memory.writeUInt8(0x20, 0);
+        // 01 000 100
+        memory.writeUInt8(0x44, 1);
+        memory.writeUInt8(0x10, 2);
+
+        cpu.next();
+        expect(cpu.ip).toEqual(3);
+        expect(cpu.read(0x10)).toEqual(0xAB & 0x11);
+    });
+
+});
