0118352224788a0f1d88a1cb149b8e786516fe7a
treelite <c.xinle@gmail.com>
2016-03-17 15:20:50 +0800

    inc

---
+++ test/incSpec.js
@@ -0,0 +1,52 @@
+/**
+ * @file INC spec
+ * @author teeelite(c.xinle@gmail.com)
+ */
+
+import CPU from '../lib/CPU';
+import {FLAG_MASK} from '../lib/const';
+
+describe('inc', () => {
+
+    let memory = new Buffer(0x100);
+    let cpu = new CPU(memory);
+
+    beforeEach(() => {
+        memory.fill(0);
+        cpu.reset();
+    });
+
+    it('memory', () => {
+        memory.writeUInt8(0x11, 0xAC);
+        memory.writeUInt8(0xFE, 0);
+        // 01 000 101
+        memory.writeUInt8(0x45, 1);
+        memory.writeUInt8(0xAC, 2);
+
+        cpu.next();
+        expect(cpu.ip).toEqual(3);
+        expect(memory[0xAC]).toEqual(0x12);
+    });
+
+    it('memory (word)', () => {
+        memory.writeUInt16LE(0xAE11, 0xAC);
+        memory.writeUInt8(0xFF, 0);
+        // 01 000 101
+        memory.writeUInt8(0x45, 1);
+        memory.writeUInt8(0xAC, 2);
+
+        cpu.next();
+        expect(cpu.ip).toEqual(3);
+        expect(cpu.read(0xAC, true)).toEqual(0xAE12);
+    });
+
+    it('register', () => {
+        cpu.dx = 0x11FF;
+        memory.writeUInt8(0x42, 0);
+
+        cpu.next();
+        expect(cpu.ip).toEqual(1);
+        expect(cpu.dx).toEqual(0x1200);
+    });
+
+});
